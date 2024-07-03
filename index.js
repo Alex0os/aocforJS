daa0d872cf2a94b0929d1cfb13dbb72f81848b1f
Matias A. <mt.alexander.go@gmail.com>
2024-07-03 15:57:41 -0400

    fix!: REPL prototype's bug fixed
    - The previous prototype has the issue of multiple calls being stack in
      the stack call, due to the async nature of the modules used to create
      the input interface. New module added to use a syncronous input
      interface

--- index.js
+++ index.js
@@ -3,33 +3,23 @@
 
 let testing = {"tomatoes": 5, "apples": 10, "bananas": 3, "pears": 0};
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const prompt = require("prompt-sync")({sigint : false});
 
 let productsQuestion = "See if we have any fruit/vegetable in our stock -> ";
 
-function askQuestion(){
 
-	// Ask for the fruit/vegetable that they're looking for
-	rl.question(productsQuestion, product => {
-		if (testing[product.toLowerCase()]) {
-			console.log(`We have ${testing[product.toLowerCase()]} of them`);
-			askQuestion()
-		} else {
-			console.log(`We are out of ${product}, sorry!`);
-			askQuestion();
-		}
-	});
+while (true){
+	let product = prompt(productsQuestion);
 
-	// Print a message when the users press "CTRL + c" to exit
-	rl.on('SIGINT', () => {
-		console.log("\nbye bye");
-		rl.close();
-	});
+	// Goodbye message if the input is "CTRL + c" or exit
+	if (!product || product === "exit") {
+		console.log("Goodbye!");
+		break;
+	}
+
+	if (testing[product.toLowerCase()]){
+		console.log(`We have ${testing[product.toLowerCase()]} of them`);
+	} else {
+		console.log(`We are out of ${product}, sorry!`);
+	}
 }
-
-askQuestion();
