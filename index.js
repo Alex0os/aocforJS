63f410ab23ba60b1c5e081e229b88d34a19c22cf
Matias A. <mt.alexander.go@gmail.com>
2024-07-03 13:17:15 -0400

    feat: add REPL prototype on "index.js" file
    - Known issues: Introducing inputs multiple times stacks the function
      calls, making it inneficient in therms of memory.

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 // This file will serve the purpose to execute all the source files and compare
 // the results that this were given with the right ones
+
+let testing = {"tomatoes": 5, "apples": 10, "bananas": 3, "pears": 0};
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let productsQuestion = "See if we have any fruit/vegetable in our stock -> ";
+
+function askQuestion(){
+
+	// Ask for the fruit/vegetable that they're looking for
+	rl.question(productsQuestion, product => {
+		if (testing[product.toLowerCase()]) {
+			console.log(`We have ${testing[product.toLowerCase()]} of them`);
+			askQuestion()
+		} else {
+			console.log(`We are out of ${product}, sorry!`);
+			askQuestion();
+		}
+	});
+
+	// Print a message when the users press "CTRL + c" to exit
+	rl.on('SIGINT', () => {
+		console.log("\nbye bye");
+		rl.close();
+	});
+}
+
+askQuestion();
