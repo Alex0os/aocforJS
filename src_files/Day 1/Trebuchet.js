626f8e6eafa74577ba4f972ef5b89802e16d4a22
Matias A. <mt.alexander.go@gmail.com>
2024-07-15 19:21:18 -0400

    feat: Day 1 (part 2) of advent of code completed

--- src_files/Day 1/Trebuchet.js
+++ src_files/Day 1/Trebuchet.js
@@ -32,6 +32,65 @@ function getNumsResult() {
 	return result;
 }
 
+// Part 2
+
+const testInput = ` two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
+
+function wordsIntoNumbers(input_buffer) {
+	input_buffer = input_buffer.split("\n");
+
+	let total = 0;
+
+	const numberWords = {
+		"zero": "0",
+		"one": "1",
+		"two": "2",
+		"three": "3",
+		"four": "4",
+		"five": "5",
+		"six": "6",
+		"seven": "7",
+		"eight": "8",
+		"nine": "9"};
+
+	input_buffer.forEach(line => {
+		if (!line) {
+			return;
+		}
+
+		let word = "";
+		numbersInLine = [];
+
+		for (char of line) {
+			if (Number(char)){
+				numbersInLine.push(char);
+				continue;
+			}
+			word += char;
+
+			for (key in numberWords) {
+				if (word.match(key)) {
+					numbersInLine.push(numberWords[key]);
+					word = char;
+					break;
+				}
+			}
+
+		}
+		const firstPlusLast = numbersInLine[0] + numbersInLine[numbersInLine.length - 1];
+		total += Number(firstPlusLast);
+	});
+	return total;
+}
+
+console.log(wordsIntoNumbers(data));
+
 module.exports = {
 	description : "This is the description",
 	result : `This is the result ---> ${getNumsResult()}`,
