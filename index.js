7c2e385e3c860a63fca06a7358429a016d8ce545
Matias A. <mt.alexander.go@gmail.com>
2024-07-16 23:03:48 -0400

    refactor: make the AOC problems stay in only one file, to avoid unnecesary complexity

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const execCommand = require("child_process").execSync;
 const prompt = require("prompt-sync")({sigint : false});
 
 
-function filesObj() {
-	const daysAndFilesObj = new Object();
+function filesArray() {
+	const daysArray = new Array();
 	let srcFilesRoutes = execCommand('find src_files/ -name "*.js" -type f', {encoding: "utf8"}).split("\n")
 	// "srcFilesRoutes" last element is an empty string that the "execComand()" returns by default
 	srcFilesRoutes = srcFilesRoutes.slice(0, -1);
@@ -13,70 +13,47 @@ function filesObj() {
 		return;
 	}
 
-	const FOLDER_INDEX = 1;
-	const FILE_INDEX = 2;
+	const FILE_INDEX = 1;
 	for (route of srcFilesRoutes) {
 		route = route.split("/");
-		let dayFolderName = route[FOLDER_INDEX];
-		let fileName = route[FILE_INDEX];
+		const fileName = route[FILE_INDEX];
+		daysArray.push(fileName);
 
-		if (daysAndFilesObj[dayFolderName]) {
-			daysAndFilesObj[dayFolderName].push(fileName);
-		} else {
-			daysAndFilesObj[dayFolderName] = [fileName];
-		}
 	}
-	return daysAndFilesObj;
+	return daysArray;
 }
 
-function commandLineDescription(daysObj){
+function commandLineDescription(daysArray){
 	let commandMessage = "---------------------------------\n"
-	for (dayFolderName in daysObj) {
-		commandMessage += `- ${dayFolderName}\n`;
+	for (let i = 0; i < daysArray.length; i++) {
+		commandMessage += `${i + 1}- ${daysArray[i]}\n`;
 	}
 	commandMessage += "---------------------------------"
 	return commandMessage;
 }
 
-function getAOCDayInfo(folderNameString, filesArray) {
-	let filesInFolder = "";
-	let separators = "*********************************"
-
-	for (let i = 0; i < filesArray.length; i++){
-		const dayNumber = i + 1;
-		filesInFolder += String(dayNumber) + "-> " + filesArray[i] + "\n";
-	}
+function getAOCDayInfo(fileString) {
+	const importedValues = require(`./src_files/${fileString}`);
+	const errorMessage = 'No values were imported from ----> ' + fileString + '\ \nCheck if a description and value are provided with the "modules.export" utility\n';
 
 	while (true) {
-		console.log("Select one of the options by its index number:\n")
-		console.log(separators + "\n" + filesInFolder + separators + "\n");
 
-		let input = prompt("Number --> ");
+		const didImport = Object.keys(importedValues).length >= 0;
+		console.log(didImport ? importedValues : errorMessage);
 
-		if (!input || input === "return") {
-			return;
-		}
-
-		const fileSelectedIndex = Number(input) - 1;
-		if (filesArray[fileSelectedIndex]){
-			const importedValues = require(`./src_files/${folderNameString}/${filesArray[fileSelectedIndex]}`);
-			const errorMessage = 'No values were imported from ----> ' + filesArray[fileSelectedIndex] + '\
-			\nCheck if a description and value are provided with the "modules.export" utility\n';
-
-			const didImport = Object.keys(importedValues).length >= 0;
-			console.log(didImport ? importedValues : errorMessage);
-		}
-		else {
-			console.log(`ERROR --> "${input}" is not a valid option`);
-		}
+		let input = prompt("Press CTRL + C or write 'return' to go back: ");
+		if (!input || input === "return")
+			break;
 	}
 }
 
 
 
+
 (function main() {
-	const daysAndFilesObj = filesObj();
-	const commandLineMessage = commandLineDescription(daysAndFilesObj);
+	const daysArray = filesArray();
+	const commandLineMessage = commandLineDescription(daysArray);
+	console.log(daysArray);
 
 	while (true){
 		console.log("Days of AOC reviewed:\n" + commandLineMessage);
@@ -90,8 +67,8 @@ function getAOCDayInfo(folderNameString, filesArray) {
 
 		console.log("\n");
 
-		if (daysAndFilesObj[daySelected]) {
-			getAOCDayInfo(daySelected, daysAndFilesObj[daySelected]);
+		if (daysArray[daySelected - 1]) {
+			getAOCDayInfo(daysArray[daySelected - 1]);
 		} else {
 			console.log(`ERROR --> "${daySelected}" is not a valid option\nPlease Try again\n`);
 		}
