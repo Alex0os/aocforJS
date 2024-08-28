e4be6e76cde1891aef51f03a015d191865169954
Matias A. <mt.alexander.go@gmail.com>
2024-08-27 23:59:26 -0400

    feat: Day 4 (part 2) of Advent Of Code completed.

--- src_files/Day4.js
+++ src_files/Day4.js
@@ -9,11 +9,8 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
 
-
-// La primera es la lista de los numeros ganadores, la segunda es la lista de
-// numeros que tienes
-
-function getGameScore(winNums, yourNums) {
+// PART 1
+function getCardScore(winNums, yourNums) {
 	let score = 0;
 
 	yourNums.forEach(num => {
@@ -30,18 +27,69 @@ function scratchCardTotal(input) {
 
 	let totalScore = 0;
 
-	for (let i = 0; i < input.length - 2; i++) {
-		let game = input[i];
+	for (let i = 0; i < input.length; i++) {
+		// For an empty line
+		if (!input[i])
+			continue;
 
-		game = game.split(":")[1];
-		game = game.split("|");
-		const winningNumbers = game[0].match(/\d+/g);
-		const yourNumbers = game[1].match(/\d+/g);
+		let cards = input[i];
 
-		totalScore += getGameScore(winningNumbers, yourNumbers);
+		cards = cards.split(":")[1];
+		cards = cards.split("|");
+		const winningNumbers = cards[0].match(/\d+/g);
+		const yourNumbers = cards[1].match(/\d+/g);
+
+		totalScore += getCardScore(winningNumbers, yourNumbers);
 	}
 	return totalScore;
 }
 
-console.log(scratchCardTotal(testInput));
+// PART 2
+
+function gamePoints(game, gameIdx) {
+	// For an empty line
+	if (!game)
+		return 0;
+
+	let score = 0;
+
+	game = game.split(":")[1];
+	game = game.split("|");
+	const winningNumbers = new Map();
+	game[0].match(/\d+/g).map(x => winningNumbers[x] = true);
+	const yourNumbers = game[1].match(/\d+/g);
+
+	yourNumbers.map(number => {
+		if (winningNumbers[number])
+			score++;
+	});
+
+	return new Array(score).fill(gameIdx + 1).map((x, i) => x + i);
+}
+
+function parseGames(input) {
+	input = input.split("\n").filter(row => row.length);
+
+	const processing = new Array(input.length).fill(0).map((_, i) => i + 1);
+	const seen = {};
+	const count = {};
+
+	while (processing.length) {
+		const idx = processing.pop();
+		count[idx] = count[idx] ? count[idx] + 1 : 1;
+		const points = seen[idx] ? seen[idx] : gamePoints(input[idx - 1], idx);
+		seen[idx] = points;
+
+		points.forEach(x => {
+			processing.push(x)
+		})
+	}
+
+	return Object.keys(count).reduce((acc, x) => { return acc + count[x] }, 0)
+}
+
+
 console.log(scratchCardTotal(fileInput));
+//console.log("totalScore", parseGames(testInput));
+console.log(parseGames(fileInput));
+//console.dir(originalsAndCopies(fileInput), {"maxArrayLength" : null});
