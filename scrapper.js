bde7d845f09566b4a87bee226994533b7a0e50a0
Matias A. <mt.alexander.go@gmail.com>
2024-08-30 12:52:45 -0400

    feat: Basic functionality of the "scrapper.js" module completed.
    - The module uses Selenium library to introduce fill the sign in form
    automatically, to then get the input of an AOC day challenge defined
    arbitrarily inside the code.

--- scrapper.js
+++ scrapper.js
@@ -1,30 +1,37 @@
-const https = require('https');
+ const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
 
-const options = {
-	hostname: 'adventofcode.com',
-	port: 443,
-	path: '/2023',
-	method: 'GET',
-	headers: {
-		'Content-Type': 'text/html'
-	},
-};
+let userName;
+let password;
 
-const req = https.request(options, (res) => {
-	console.log(`STATUS: ${res.statusCode}\n\n`);
-	console.log(`HEADERS: ${JSON.stringify(res.headers)}\n\n`);
-	res.setEncoding('utf8');
-	res.on('data', (chunk) => {
-		console.log(`BODY: ${chunk}\n\n`);
-	});
-	res.on('end', () => {
-		console.log('No more data in response.');
-	});
-});
+async function getInput(driver, day) {
+	const notValid = !day || (0 > parseInt(day) || parseInt(day) > 26)
+	if (notValid) {
+		console.log("You entered an invalid day number");
+		return;
+	}
 
-req.on('error', (e) => {
-	console.error(`problem with request: ${e.message}`);
-});
+	await driver.get("https://adventofcode.com/2023/day/" + day + "/input");
+	const challengeInput = await driver.findElement(By.css("pre")).getText();
+	console.log(challengeInput);
+}
 
-// Write data to request body
-req.end();
+
+(async function example() {
+	let driver = new Builder()
+		.forBrowser(Browser.FIREFOX)
+		.build();
+
+	await driver.get("https://adventofcode.com/auth/github");
+
+	const emailInput = await driver.findElement(By.name("login"));
+	const passwordInput = await driver.findElement(By.name("password"));
+	const submitButton = await driver.findElement(By.name("commit"));
+
+	await emailInput.sendKeys(userName);
+	await passwordInput.sendKeys(password);
+	await submitButton.click();
+
+	await driver.wait(until.titleIs("Advent of Code 2023"), 10000)
+	await getInput(driver, 1);
+	await driver.quit();
+})();
